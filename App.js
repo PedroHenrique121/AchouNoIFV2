b7774e2cadb30f52fcf915fd2323312622a42c93
Romulo <roargs2009@gmail.com>
2026-09-17 11:03:30 -0300

    Atualizado

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
+import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   StyleSheet,
@@ -9,20 +8,13 @@ import {
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./src/config/firebase";
 
-import LoginScreen from "./src/screens/LoginScreen";
 import CadastroScreen from "./src/screens/CadastroScreen";
 import EsqueciSenhaScreen from "./src/screens/EsqueciSenhaScreen";
-=======
-import { ActivityIndicator, View, StyleSheet } from "react-native";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./src/config/firebase";
-import LoginScreen from "./src/screens/LoginScreen";
->>>>>>> 76bc0a53c6c01c8f29dea2d54a309ed71f2dd73c
 import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   const [user, setUser] = useState(undefined);
-<<<<<<< HEAD
   const [tela, setTela] = useState("login");
 
   useEffect(() => {
@@ -44,23 +36,10 @@ export default function App() {
           size="large"
           color="#2563eb"
         />
-=======
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, setUser);
-    return unsubscribe;
-  }, []);
-
-  if (user === undefined) {
-    return (
-      <View style={styles.loading}>
-        <ActivityIndicator size="large" color="#2563eb" />
->>>>>>> 76bc0a53c6c01c8f29dea2d54a309ed71f2dd73c
       </View>
     );
   }
 
-<<<<<<< HEAD
   // Usuário está logado
   if (user) {
     return <HomeScreen user={user} />;
@@ -90,12 +69,6 @@ export default function App() {
       onCadastrar={() => setTela("cadastro")}
       onEsqueciSenha={() => setTela("esqueciSenha")}
     />
-=======
-  return user ? (
-    <HomeScreen user={user} />
-  ) : (
-    <LoginScreen onLoginSuccess={setUser} />
->>>>>>> 76bc0a53c6c01c8f29dea2d54a309ed71f2dd73c
   );
 }
 
@@ -104,9 +77,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-<<<<<<< HEAD
     backgroundColor: "#fff",
-=======
->>>>>>> 76bc0a53c6c01c8f29dea2d54a309ed71f2dd73c
   },
 });
